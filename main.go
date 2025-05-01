d30503f6b001823c47d87a40c3427e37b3cc8137
Chinh (KC) Do <contact@doinby.co>
2025-05-01 14:29:27 +1000

    more sum

--- main.go
+++ main.go
@@ -5,4 +5,9 @@ func main() {
 	// random.Random()
 	// choose_your_own_adventure.Cave()
 
+	// len() and cap()
+	// value := make([]int, 1, 5)
+	// fmt.Println(value)
+	// fmt.Println(len(value))
+	// fmt.Println(cap(value))
 }
--- sum.go
+++ sum.go
@@ -10,3 +10,33 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// sums := make([]int, lengthOfNumbers)
+
+	// Starts with empty slice of sums
+	var sums []int
+
+	// and append calculated value to sums
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
